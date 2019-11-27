3a1e442d63479df44b7aaf9705d91468d5fd375e
Daniel Orio <danielislas3@live.com>
2019-11-27 11:51:59 -0600

    busqueda por estado

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const db = require('./padron.json')
 const jsonQuery = require('json-query')
 const data = db.data
+
+
+//busqueda por estado
+console.log(
+  jsonQuery('data[**][*ESTADO=JAL]', {data: db}).value
+)
