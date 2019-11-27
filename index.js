6e29fb1f97dc2473fc8cf411e5618a321cc607e1
Daniel Orio <danielislas3@live.com>
2019-11-27 13:14:31 -0600

    funcion asignar NIR

--- index.js
+++ index.js
@@ -2,8 +2,62 @@ const db = require('./padron.json')
 const jsonQuery = require('json-query')
 const data = db.data
 
+const nir, serie, numIni, numFin
+/*
+ *ESTRUCTURA DEL NUMERO (todos los numeros son de 10 digitos)
+ * NIR(lada) + serie + NUMERACION_INICIAL + NUMERACION_FINAL
+ * ->NIR puede ser de 2 o 3 digitos
+ * para 2 digitos (33,55,56,81)
+ * para 3 digitos (2,31,32-9,4,58,59,6,7,82-9,9)
+ *->SERIE  puede ser de 3 o 4 digitos 
+ * si el NIR es de 2 digitos la serie es de 4
+ * si el NIR es de 3 digitos la serie es de 3
+ *
+ */
+
 
 //busqueda por estado
-console.log(
-  jsonQuery('data[**][*ESTADO=JAL]', {data: db}).value
-)
+// console.log(
+//   jsonQuery('data[**][*ESTADO=JAL]', {
+//     data: db
+//   }).value
+// )
+
+function assignNIR(num) {
+  num = num.toString()
+  num = num.split('')
+
+
+  switch (num[0]) {
+    case '0':
+      return 'Numero invalido'
+      break
+    case '1':
+      return 'Numero invalido'
+      break
+
+    case '3': {
+      if (num[1] == 3) return num[0] + num[1]
+      return num[0] + num[1] + num[2]
+    }
+    break
+    case '4':
+     return (num[0] + num[1] + num[2])
+    break
+    case '5': {
+      if (num[1] == 5 || num[1] == 6) return num[0] + num[1]
+      return num[0] + num[1] + num[2]
+    }
+      break
+    case '8': {
+      if (num[1] == 0) return 'Numero invalido'
+      if (num[1] == 1) return num[0] + num[1]
+      return num[0] + num[1] + num[2]
+    }
+    break
+    default:
+    return (num[0] + num[1] + num[2])
+  }
+
+  return num
+}
